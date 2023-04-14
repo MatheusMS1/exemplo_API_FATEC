d20ef09ac8e405fe575a74ada595e3b1dcbd6a73
MatheusMS1 <msmatheus407@gmail.com>
2023-04-14 20:15:17 -0300

    remove "import express from express" do código

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require ('express')
-import express from express
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req,res) => {
-    res.send('hello world')
+    res.send('Hello World')
 })
 
 app.listen(port, () => {
